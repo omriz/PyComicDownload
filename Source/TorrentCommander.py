30d6716dbcdbf80bf39730569f0821771153766f
omriz <omri.zohar@gmail.com>
2013-01-21 00:46:47 +0200

    Adding filtering for torrent content and modifying the way torrents are added, removed and managed

--- Source/TorrentCommander.py
+++ Source/TorrentCommander.py
@@ -5,6 +5,9 @@ Torrent Commander class
 
 import transmissionrpc
 import json
+import re
+from time import sleep
+from exceptions import Exception
 
 class TorrentCommander(object):
     """
@@ -25,23 +28,61 @@ class TorrentCommander(object):
                                 )
         self.download_dir = conf['download_dir']
 
-    def add_torrents(torrents, download_dir=None, file_filter=None):
+    def add_torrents(self,torrents, download_dir=None, file_filter=None):
         """
         Torrents should be a list
         If passed a string it will be downloaded
-        file_filter will filter the files we want to download
+        file_filter will filter the files we want to download - this will be a list of strings that should match the file names
+        example:
+            ['Aquaman', 'Superman', 'Justice.*league']
         """
         if not isinstance(torrents,(tuple,list)):
             assert isinstance(torrents,basestring)
             torrents = [torrents]
-        download_dir = self.download_dir if download_dir is None
+        if download_dir is None: download_dir = self.download_dir 
         for torrent in torrents:
-            running_torrent=self.transmission.add_torrent(torrent,download_dir = self.download_dir,paused=True)
-            self.activate_torrent(running_torrent,file_filter)
+            if torrent.startswith("magnet"): # Handling magnet links
+                running_torrent=self.transmission.add_torrent(None,filename=torrent,download_dir = self.download_dir)
+            else:
+                running_torrent=self.transmission.add_torrent(torrent,download_dir = self.download_dir)
+            self.filter_torrent(running_torrent,file_filter)
 
-    def activate_torrent(torrent,file_filter):
-    #TODO - needs implementation
-        pass
+    def filter_torrent(self,torrent,file_filter):
+        """
+        This function will take a torrent and select only the relevant files on it
+        if there's nothing to use in it, it will remove it from the queue
+        """
+        try:
+            files_dict = self.wait_for_files(torrent,timeout=5*60)
+            files = []
+            for file_key in files_dict.keys():
+                for file_name in file_filter:
+                    if re.match(file_name,files_dict[file_key]['name'],re.IGNORECASE) is not None:
+                        files.append(file_key)
+            if not files: raise NoFilesException
+            self.transmission.change(torrent.fields['id'], files_wanted = files)
+            self.transmission.start(torrent['id'])
+        except NoFilesException:
+            self.transmission.remove(torrent['id'], delete_data=True)
+
+    def wait_for_files(self,torrent,timeout=5*60):
+        """
+        Activates a torrent if needed.
+        Waits till it reports back its list of files.
+        """
+        iterations = timeout/5
+        if torrent.status == "stopped":
+            self.transmission.start(torrent.fields['id'])
+        while iterations:
+            files_dict = self.transmission.get_files(torrent.fields['id'])[torrent.fields['id']]
+            if files_dict: return files_dict
+            sleep(5)
+            iterations-=1
+        raise NoFilesException
+
+class NoFilesException(Exception):
+    def __str__(self):
+        return "No files found"
 
 if __name__ == '__main__':
     print "Beginning TorrentCommander unit test"
